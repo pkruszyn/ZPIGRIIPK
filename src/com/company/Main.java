91a34344666686d15545c398e08bf20d1c0ab239
pkruszyn <pkruszynski@student.kul.lublin.pl>
2022-03-15 17:15:29 +0100

    Changes in Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Pierwsza linia z gałęzi Main");
+        System.out.println("Druga linia z gałęzi Main");
 	// write your code here
     }
 }
